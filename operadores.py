351324fc4395cf098626b46d74a672d9932da978
JHONNYS MAX <jhonnysmax2@gmail.com>
2022-11-15 12:20:47 -0300

    Teste de Aula de Python

---
+++ operadores.py
@@ -0,0 +1,7 @@
+saldo = 2000.0
+saque = float(input("Informe o valor do saque: "))
+
+if saldo >= saque:
+    print("Saque realizado!")
+else:
+    print("Saldo insuficiente!")
---
+++ primeiro programa.py
@@ -0,0 +1,6 @@
+print("Hello world!")
+print(1+2)
+
+saldo = 450
+saque = 200
+print(saldo = saque)
